ba11495cb5051b79858b6fc4695e5e5758eb7310
kert <franciskertdn@gmail.com>
2025-12-06 21:31:00 +0800

    Done header

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
-import { useState } from "react";
-import { FaTasks } from "react-icons/fa";
+import Header from "./components/Header.jsx";
 import "./App.css";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (item) => setTodos([...todos, item]);
-  const deleteTodo = (index) => setTodos(todos.filter((_, i) => i !== index));
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center", // horizontal center
-        justifyContent: "center", // vertical center
-        padding: "20px",
-      }}
-    >
-      <h1
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "8px",
-          marginBottom: "30px",
-        }}
-      >
-        <FaTasks size={24} color="#4CAF50" /> Todo List
-      </h1>
-      <div style={{ width: "100%", maxWidth: "400px" }}>
-        <TodoInput addTodo={addTodo} />
-        <TodoList todos={todos} deleteTodo={deleteTodo} />
-      </div>
-    </div>
+    <>
+      <Header />
+
+      <section id="home" style={{ height: "100vh", paddingTop: "80px" }}>
+        <h1 class=" text-danger">Home Section</h1>
+        <p>Welcome to our website!</p>
+      </section>
+
+      <section id="about" style={{ height: "100vh", paddingTop: "80px", background: "#f8f9fa" }}>
+        <h1 class="text-success">About Section</h1>
+        <p>This is the About section of the website.</p>
+      </section>
+
+      <section id="contact" style={{ height: "100vh", paddingTop: "80px" }}>
+        <h1>Contact Section</h1>
+        <p>Contact us here.</p>
+      </section>
+    </>
   );
 }
 
